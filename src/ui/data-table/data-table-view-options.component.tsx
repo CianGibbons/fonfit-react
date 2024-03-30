8894dcc44f19145df35d45ae11c04829697ded03
Cian Gibbons <ciangibb@gmail.com>
2024-03-30 23:58:31 +0000

    Fixed In Stock display name in view options

--- src/ui/data-table/data-table-view-options.component.tsx
+++ src/ui/data-table/data-table-view-options.component.tsx
@@ -40,7 +40,7 @@ export function DataTableViewOptions<TData>({ table }: DataTableViewOptionsProps
                 checked={column.getIsVisible()}
                 onCheckedChange={(value) => column.toggleVisibility(!!value)}
               >
-                {column.id}
+                {column.id.replace(/_/g, ' ')}
               </DropdownMenuCheckboxItem>
             );
           })}
